5b0b9bcbc28d46ee4e8c242713739f9637a3d244
Christopher Stone <chrisrunner542@gmail.com>
2025-04-17 11:59:07 -0700

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by MacBook on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
